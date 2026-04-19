11c35b546f434fa871023a542b4e0f1ee3e16cde
Thomas-Ring <ringthomas4321@gmail.com>
2026-04-19 13:20:09 -0400

    Ajouté validation du formulaire

--- javaScript.js
+++ javaScript.js
@@ -1,4 +1,5 @@
 $("#boutonSoumettre").on("click", (function(){
+    if (($("#titre").val() !== "") && ($("#genre").val() !== "") && ($("#statut").val() !== "")) {
         $("#mesFilmsSeries").append("<tr> <td> " + $("#titre").val() + "</td> <td> " + $("#genre").val() + "</td> <td class='statutTableau'> " + $("#statut").val() + "</td> <td> <button type='button' class='delete'>Supprimer</button></td></tr>");
         $("#titre").val("");
         $("#genre").val("");
@@ -13,21 +14,25 @@ $("#boutonSoumettre").on("click", (function(){
         $("#nbVu").html($(".statutTableau").filter(function(index){
             return $(this).text().trim() === "Vu";
         }).length);
-    }))
+        $("#avertissementRequired").text("");
+    } else {
+        $("#avertissementRequired").text("Veuillez remplir tous les champs.");
+    }
+}))
 
 $("#mesFilmsSeries").on("click", ".delete", (function(){
-        $(this).parent().parent().children().remove();
-        $("#nbTotal").html($(".statutTableau").length);
-        $("#nbAVoir").html($(".statutTableau").filter(function(index){
-            return $(this).text().trim() === "À voir";
-        }).length);
-        $("#nbEnCours").html($(".statutTableau").filter(function(index){
-            return $(this).text().trim() === "En cours";
-        }).length);
-        $("#nbVu").html($(".statutTableau").filter(function(index){
-            return $(this).text().trim() === "Vu";
-        }).length);
-    }));
+    $(this).parent().parent().children().remove();
+    $("#nbTotal").html($(".statutTableau").length);
+    $("#nbAVoir").html($(".statutTableau").filter(function(index){
+        return $(this).text().trim() === "À voir";
+    }).length);
+    $("#nbEnCours").html($(".statutTableau").filter(function(index){
+        return $(this).text().trim() === "En cours";
+    }).length);
+    $("#nbVu").html($(".statutTableau").filter(function(index){
+        return $(this).text().trim() === "Vu";
+    }).length);
+}));
 
 $("#filtre").on("change", function(){
     if ($("#filtre").val() === "Tous") {
