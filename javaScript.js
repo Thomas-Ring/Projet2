2f62c7b6334f403bad769ba24396d1795b2afeaf
Thomas-Ring <ringthomas4321@gmail.com>
2026-04-19 09:46:46 -0400

    Ajouté fonctionnalité d'ajouter des films au tableau, qui peuvent ensuite être supprimés

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,10 @@
-$(document).ready(function() {
-    $(".delete").click(function(){
-        $(this).parent().parent().empty();
-    });
-});
+$("#boutonSoumettre").on("click", (function(){
+        $("#mesFilmsSeries").append("<tr> <td> " + $("#titre").val() + "</td> <td> " + $("#genre").val() + "</td> <td> " + $("#statut").val() + "</td> <td> <button type='button' class='delete'>Supprimer</button></td></tr>");
+        $("#titre").val("");
+        $("#genre").val("");
+        $("#statut").val("");
+    }))
+
+$("#mesFilmsSeries").on("click", ".delete", (function(){
+        $(this).parent().parent().children().remove();
+    }));
