085b84b284538977d89670c6600e2e0e63f833ea
Thomas-Ring <ringthomas4321@gmail.com>
2026-04-19 10:47:00 -0400

    Ajouté stats qui se mettent à jour automatiquement

--- javaScript.js
+++ javaScript.js
@@ -1,10 +1,30 @@
 $("#boutonSoumettre").on("click", (function(){
-        $("#mesFilmsSeries").append("<tr> <td> " + $("#titre").val() + "</td> <td> " + $("#genre").val() + "</td> <td> " + $("#statut").val() + "</td> <td> <button type='button' class='delete'>Supprimer</button></td></tr>");
+        $("#mesFilmsSeries").append("<tr> <td> " + $("#titre").val() + "</td> <td> " + $("#genre").val() + "</td> <td class='statutTableau'> " + $("#statut").val() + "</td> <td> <button type='button' class='delete'>Supprimer</button></td></tr>");
         $("#titre").val("");
         $("#genre").val("");
         $("#statut").val("");
+        $("#nbTotal").html($(".statutTableau").length);
+        $("#nbAVoir").html($(".statutTableau").filter(function(index){
+            return $(this).text().trim() === "À voir";
+        }).length);
+        $("#nbEnCours").html($(".statutTableau").filter(function(index){
+            return $(this).text().trim() === "En cours";
+        }).length);
+        $("#nbVu").html($(".statutTableau").filter(function(index){
+            return $(this).text().trim() === "Vu";
+        }).length);
     }))
 
 $("#mesFilmsSeries").on("click", ".delete", (function(){
         $(this).parent().parent().children().remove();
+        $("#nbTotal").html($(".statutTableau").length);
+        $("#nbAVoir").html($(".statutTableau").filter(function(index){
+            return $(this).text().trim() === "À voir";
+        }).length);
+        $("#nbEnCours").html($(".statutTableau").filter(function(index){
+            return $(this).text().trim() === "En cours";
+        }).length);
+        $("#nbVu").html($(".statutTableau").filter(function(index){
+            return $(this).text().trim() === "Vu";
+        }).length);
     }));
