04f3786b7c15be0486226033799276f76f0900f9
Thomas-Ring <ringthomas4321@gmail.com>
2026-04-19 15:39:02 -0400

    Rendu les menus déroulants comme dans la maquette

--- javaScript.js
+++ javaScript.js
@@ -1,9 +1,9 @@
 $("#boutonSoumettre").on("click", (function(){
-    if (($("#titre").val() !== "") && ($("#genre").val() !== "") && ($("#statut").val() !== "")) {
+    if (($("#titre").val() !== "") && ($("#genre").val() !== "Choisir un genre") && ($("#statut").val() !== "Choisir un statut")) {
         $("#mesFilmsSeries").append("<tr> <td> " + $("#titre").val() + "</td> <td> " + $("#genre").val() + "</td> <td class='statutTableau'> " + $("#statut").val() + "</td> <td> <button type='button' class='delete'>Supprimer</button></td></tr>");
         $("#titre").val("");
-        $("#genre").val("");
-        $("#statut").val("");
+        $("#genre").val("Choisir un genre");
+        $("#statut").val("Choisir un statut");
         $("#nbTotal").html($(".statutTableau").length);
         $("#nbAVoir").html($(".statutTableau").filter(function(index){
             return $(this).text().trim() === "À voir";
